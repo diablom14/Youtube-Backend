8c34446275b23547c880ed354168efc83b62fb5e
diablom14 <divyanshuasadeveloper@gmail.com>
2025-06-17 21:33:53 +0530

    Route checking done

---
+++ src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js";
+const route = Router()
+
+route.post("/register", registerUser)
+
+export default route
